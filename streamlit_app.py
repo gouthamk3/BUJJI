d714c2e15e0cd82a335892092da92508b2e3ee7b
Goutham <gouthamk36663@gmail.com>
2024-12-23 10:40:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,15 +82,15 @@ def main():
         user_input(user_question) 
 
 #To Upload New Document remove # 
-    #with st.sidebar:
-     #   st.title("Menu:")
-      #  pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
-       # if st.button("Submit & Process", key="process_button"):  # Check if API key is provided before processing
-        #    with st.spinner("Processing..."):
-         #       raw_text = get_pdf_text(pdf_docs)
-          #      text_chunks = get_text_chunks(raw_text)
-           #     get_vector_store(text_chunks, OPENAI_API_KEY)
-            #    st.success("Done")
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
+        if st.button("Submit & Process", key="process_button"):  # Check if API key is provided before processing
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks, OPENAI_API_KEY)
+                st.success("Done")
                
 if __name__ == "__main__":
     main()
